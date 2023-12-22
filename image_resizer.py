39a7cb7e6c25a2811a3ce20f4568349261cb7af7
Kunyelewosi Alexander <kunalexander360@gmail.com>
2023-12-22 12:11:10 +0000

    Initial commit

---
+++ image_resizer.py
@@ -0,0 +1,9 @@
+"""
+# Install pillow fi you haven't
+# import pillow
+# open up iamge we want to resize using python
+# print current size
+3specify the size we want to change it to
+# save the new resized image
+"""
+
