dd8a0e9dfe38f289d6940532a9abc33a8c85e339
Kunyelewosi Alexander <kunalexander360@gmail.com>
2023-12-22 12:32:27 +0000

    Final commit

--- image_resizer.py
+++ image_resizer.py
@@ -1,5 +1,5 @@
 """
-# Install pillow fi you haven't
+# Install pillow if you haven't
 # import pillow
 # open up iamge we want to resize using python
 # print current size
@@ -7,3 +7,24 @@
 # save the new resized image
 """
 
+from PIL import Image
+image = Image.open("bookshelf.png")
+
+
+
+def resize_image(width, height):
+    image = Image.open("bookshelf.png")
+
+    print(f"Current size: {image.size}")
+
+    resized_image = image.resize((width, height))
+
+    resized_image.save(f"komla-alex-image-resize{resized_image.size}.png")
+
+    print(f"New size: {resized_image.size}")
+
+
+width = int(input("Input new width: "))
+height = int(input ("Enter height: "))
+
+resize_image(width, height)
